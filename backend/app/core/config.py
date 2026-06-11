913483ae1753852f96ce7405244654139da59e1a
Walterwhite10151 <MARTINPATEL.ICT24@adaniuni.ac.in>
2026-06-11 10:54:48 +0530

    Fix ALLOWED_ORIGINS hardcoded in config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -23,10 +23,11 @@ class Settings(BaseSettings):
 
     # CORS
     ALLOWED_ORIGINS: List[str] = [
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:5173",
-    ]
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:5173",
+    "https://main.d37oqief8o78ft.amplifyapp.com",
+]  
 
     # Admin
     ADMIN_EMAIL: str = "admin@resumeanalyzer.com"
